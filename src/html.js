2626e201545440ec49365a73d23904580b1e7d6f
morr0564 <58083681+chrisnmorrison@users.noreply.github.com>
2022-07-13 14:52:11 -0400

    fix script.. again

---
+++ src/html.js
@@ -0,0 +1,51 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+      <script
+  dangerouslySetInnerHTML={{
+    __html: `
+    document.addEventListener('scroll', e => {
+      const scroll = document.documentElement.scrollTop
+      if (scroll >= 100) {
+        document.querySelector('body').classList.add('scroll')
+      } else {
+        document.querySelector('body').classList.remove('scroll')
+      }
+    })
+    
+        `,
+  }}
+/>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
