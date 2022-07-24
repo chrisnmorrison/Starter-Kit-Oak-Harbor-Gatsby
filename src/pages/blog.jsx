a32ebe906709f27256968404455a4f4af2256f2c
Chris Morrison <chrnatmor@gmail.com>
2022-07-24 16:59:57 -0400

    Update blog.jsx

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import Posts from '../components/blog'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const Blog = () => {
-  return (
-    <Posts/>
-  )
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      allMarkdownRemark(
+        limit: 10
+        sort: { fields: frontmatter___date, order: DESC }
+      ) {
+        nodes {
+          excerpt(pruneLength: 50)
+          frontmatter {
+            category
+            date
+            image {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+          id
+        }
+      }
+    }
+  `)
+  const { posts } = data.allMarkdownRemark.nodes
+  return <Posts posts={posts} />
 }
 
 export default Blog
